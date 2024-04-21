d2da26d05ccb50824d2211e98b110f42f1b636f5
NeelaReddy <neelareddy.i25@gmail.com>
2024-04-21 20:49:22 +0530

    Good forever

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -26,3 +26,16 @@ then
 else
     echo "you are super user."
 fi
+
+for i in $@
+do
+   echo "Package to install: $i"
+   dnf list installed $i &>>$LOGFILE
+   if [ $? -eq 0 ]
+   then
+       echo -e "$i already installed...$Y SKIPPING $N"
+    else
+        dnf install $i -y &>>$LOGFILE
+        VALIDATE $? "Installation of $i"
+    fi    
+done   
