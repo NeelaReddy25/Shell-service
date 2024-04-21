66168196752fa3101dbbd16f15818ed3aa9ce3f2
NeelaReddy <neelareddy.i25@gmail.com>
2024-04-21 22:50:25 +0530

    Good forever

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -21,10 +21,10 @@ VALIDATE(){
 
 if [ $USERID -ne 0 ]
 then
-    echo "Please run this script with root access"
+    echo "Please run this script with root access."
     exit 1 #manually exit if error comes
 else
-    echo "You are super user"
+    echo "You are super user."
 fi
 
 for i in $@
