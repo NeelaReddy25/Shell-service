740dde77dae0e001acb54cb21c82261d0a6f8a1f
NeelaReddy <neelareddy.i25@gmail.com>
2024-04-21 17:39:38 +0530

    Good forever

--- 10-Installation.sh
+++ 10-Installation.sh
@@ -4,9 +4,9 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-    echo "Please run this script with root access"
+    echo "Please run this script with root access."
 else
-    echo "You are super user"
+    echo "You are super user."
 fi
-        
+
 dnf install mysql -y
