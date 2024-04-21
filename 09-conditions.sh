696e79d8a2e353bac4e0e798e821e2436fd7c86e
NeelaReddy <neelareddy.i25@gmail.com>
2024-04-21 17:03:54 +0530

    Good forever

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -6,5 +6,7 @@ if [ $NUMBER -gt 10 ]
 then
     echo "Given number $NUMBER is greater than 10"
 else
-    echo "Given number $NUMBER is less than 10"
+    echo "Given number $NUMBER is less than 10" 
 fi        
+
+
