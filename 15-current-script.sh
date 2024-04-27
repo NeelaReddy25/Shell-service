495981d1bf95ca99a72549548b496d2df6436154
NeelaReddy <neelareddy.i25@gmail.com>
2024-04-27 15:16:01 +0530

    Doing forever

---
+++ 15-current-script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+COURSE="Devops from current script"
+
+ech0 "Before calling other sript, course: $COURSE"
+echo "Process ID of current shell script: $$"
+
+# ./16-other-script.sh
+
+echo "After calling other script, course: $COURSE"
