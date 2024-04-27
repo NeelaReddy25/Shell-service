6a608da8c000f45145a95eabed46d8e3c342e958
NeelaReddy <neelareddy.i25@gmail.com>
2024-04-27 17:59:59 +0530

    Doing forever

--- 17-set.sh
+++ 17-set.sh
@@ -6,9 +6,9 @@ failure(){
     echo "Failed at $1: $2"
 }
 
-trap 'failure ${LINENO} "BASH_COMMAND"' ERR
+trap 'failure ${LINENO} "$BASH_COMMAND"' ERR
 
-USERID=(id -u)
+USERID=$(id -u)
 
 if [ $USERID -ne 0 ] #ERR
 then 
