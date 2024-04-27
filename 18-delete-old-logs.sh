52887f15ac2785a37ccf51dda2c114df369d664a
NeelaReddy <neelareddy.i25@gmail.com>
2024-04-27 18:52:57 +0530

    Doing forever

---
+++ 18-delete-old-logs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SOURCE_DIRECTORY=/tmp/app-logs
+
+mkdir -d "$SOURCE_DIRECTORY"
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ -d $SOURCE_DIRECTORY ]
+then
+    echo -e "$G Source directory exists $N"
+else
+    echo -e "$R Please make sure $SOURCE_DIRECTORY exists $N"
+    exit 1
+fi        
