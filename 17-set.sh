849c1e567923e22fa84e6428b7d0a0e043288179
NeelaReddy <neelareddy.i25@gmail.com>
2024-04-27 17:57:06 +0530

    Doing forever

---
+++ 17-set.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+failure(){
+    echo "Failed at $1: $2"
+}
+
+trap 'failure ${LINENO} "BASH_CAMMAND"' ERR
+
+USERID=(id -u)
+
+if [ $USERID -ne 0 ] #ERR
+then 
+    echo "Please run this script with root access."
+    exit 1 #manually exit if error comes.
+else
+    echo "You are super user."
+
+fi
+
+dnf install mysql -y &>>$LOGFILE
+dnf install git -y &>>$LOGFILE
+
+echo "is script proceeding?"
